6bb18151a700ff78df446295f73e3783d3d7341b
Hakan Caldag <148437420+caldag@users.noreply.github.com>
2025-05-23 15:27:48 +0100

    Delete oomph-lib FEM code directory

--- oomph-lib FEM code/user_src/adv_diff_time_wind/adv_diff_time_wind.h
+++
@@ -1,5 +0,0 @@
-// This file was generated automatically during the make process
-// and it will be remade automatically
-#include<adv_diff_time_wind/hello_world.h> 
-#include<adv_diff_time_wind/gen_advection_diffusion_elements_time_wind.h> 
-#include<adv_diff_time_wind/refineable_gen_advection_diffusion_elements_time_wind.h> 
--- oomph-lib FEM code/user_src/adv_diff_time_wind/jack_cool.h
+++
@@ -1,3 +0,0 @@
-// This file was generated automatically during the make process
-// and it will be remade automatically
-#include<jack_cool/hello_world.h> 
